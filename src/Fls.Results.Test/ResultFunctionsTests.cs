57bbc8e98d9b1152bc4e5f7ecb9317c9091cfd6e
Lev <l.kargalov@yandex.ru>
2020-03-07 16:49:15 +0300

    Added unit test, MathchAsync for SuccessResul

--- src/Fls.Results.Test/ResultFunctionsTests.cs
+++ src/Fls.Results.Test/ResultFunctionsTests.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using Moq;
+using System.Threading.Tasks;
 
 namespace Fls.Results.Test
 {
@@ -53,5 +54,22 @@ namespace Fls.Results.Test
 
             Assert.Equal(expectedResult, actualResult);
         }
+
+        [Fact]
+         public async void SuccessMatchAsyncTest()
+         {
+             var testValue = 2;
+             var successBound = new Mock<IOperationResult<float>>().Object;
+             var errorBound = new Mock<IOperationResult<float>>().Object;
+             var failureBound = new Mock<IOperationResult<float>>().Object;
+             Func<int, Task<IOperationResult<float>>> bindSuccess = _ => Task.FromResult(successBound);
+             Func<string, Task<IOperationResult<float>>> bindError = _ => Task.FromResult(errorBound);
+             Func<Exception, Task<IOperationResult<float>>> bindFailure = _ =>  Task.FromResult(failureBound);
+
+             var sut = new OperationResult.SuccessResult<int>(testValue);
+
+             var matchResult = await sut.MatchAsync(bindSuccess, bindError, bindFailure);
+             Assert.Equal(successBound, matchResult);
+         }
     }
 }
