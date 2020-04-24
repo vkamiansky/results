40362771a655ff2231e2c8e963ab962819b1fd1d
Deimos <l.kargalov@yandex.ru>
2020-04-24 17:13:19 +0300

    fix 2

--- src/Fls.Results/OperationResult.cs
+++ src/Fls.Results/OperationResult.cs
@@ -201,16 +201,7 @@ namespace Fls.Results
                 return aList.ToResult();
             }))).Bind(x => x.ToArray().ToResult());
         }
-
-        public static IOperationResult<T[]> Any<T>(this IEnumerable<Func<IOperationResult<T>>> source, T returnAsError)
-        {
-            return source.Aggregate(new List<T>().ToResult(), (a, f) => a.Bind(aList => f().IfError(() => Success(returnAsError)).Bind(newRes =>
-            {
-                    aList.Add(newRes);
-                    return aList.ToResult();
-            }))).Bind(x => x.ToArray().ToResult());
-        }
-
+        
         public static IOperationResult<T[]> Any<T>(this IEnumerable<Func<IOperationResult<T>>> source)
         {
             return source.Aggregate(new List<T>().ToResult(), (a, f) => a.Bind(aList => f().Bind(newRes =>
