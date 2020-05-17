b07832172206333e464429f25eb07f691689d3e0
Vitaly Kamiansky <vitaly.kamiansky@firstlinesoftware.com>
2020-05-17 12:38:04 +0300

    Try running PS build script

--- build/Build.cs
+++ build/Build.cs
@@ -55,7 +55,7 @@ class Build : NukeBuild
 
     Target Compile => _ => _
         .DependsOn(Restore)
-        .Executes(() => Console.WriteLine("Nuget key length is:" + NugetKey))
+        .Executes(() => Console.WriteLine("Nuget key length is:" + NugetKey.Length))
         .Executes(() =>
         {
             DotNetBuild(s => s
